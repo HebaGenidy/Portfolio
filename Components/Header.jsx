61e6571c91c65c80a73328c9398501f1884418df
Heba <hebamohamed25801@gmail.com>
2024-11-07 17:26:13 +0200

    Merged all recent changes and updates

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import 'react'
 import './Header.css'
+import me from '../src/assets/me.png'
 
 function Header() {
   return (
     <div className='cls-header'>
-        <img src="../src/assets/me.png" alt="" />
+        <img src={me} alt="" />
         <h1><span>I'm Heba Mohamed, </span>frontend developer based in Egypt.</h1>
         <p>I'm frontend developer from Egypt ,with 1 year of experience.</p>
         <div className="cls-header-action">
